982a88271d246c9f9a68a9b6b08914b43f20d640
Aleksandr <38476175+MarchenkoOleksandr@users.noreply.github.com>
2018-06-21 20:50:32 +0300

    Add files via upload

---
+++ WinForms_homework1_5_runaway_static/WinForms_homework1_5_runaway_static/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace WinForms_homework1_5_runaway_static
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.ButtonYes = new System.Windows.Forms.Button();
+            this.ButtonNo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(28, 12);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(709, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Чи задоволені Ви своєю зарплатою?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // ButtonYes
+            // 
+            this.ButtonYes.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonYes.Location = new System.Drawing.Point(597, 392);
+            this.ButtonYes.Name = "ButtonYes";
+            this.ButtonYes.Size = new System.Drawing.Size(75, 40);
+            this.ButtonYes.TabIndex = 2;
+            this.ButtonYes.TabStop = false;
+            this.ButtonYes.Text = "Так";
+            this.ButtonYes.UseVisualStyleBackColor = true;
+            this.ButtonYes.Click += new System.EventHandler(this.ButtonYes_Click);
+            // 
+            // ButtonNo
+            // 
+            this.ButtonNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ButtonNo.Location = new System.Drawing.Point(691, 392);
+            this.ButtonNo.Name = "ButtonNo";
+            this.ButtonNo.Size = new System.Drawing.Size(75, 40);
+            this.ButtonNo.TabIndex = 1;
+            this.ButtonNo.TabStop = false;
+            this.ButtonNo.Text = "Ні";
+            this.ButtonNo.UseVisualStyleBackColor = true;
+            this.ButtonNo.MouseEnter += new System.EventHandler(this.ButtonNo_MouseEnter);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(778, 444);
+            this.Controls.Add(this.ButtonNo);
+            this.Controls.Add(this.ButtonYes);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Питання";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ButtonYes;
+        private System.Windows.Forms.Button ButtonNo;
+    }
+}
+
---
+++ WinForms_homework1_5_runaway_static/WinForms_homework1_5_runaway_static/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WinForms_homework1_5_runaway_static
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonYes_Click(object sender, EventArgs e)
+        {
+            label1.Text = "Чомусь ми так і думали! ;-)\n   З повагою, адміністрація";
+        }
+
+        private void ButtonNo_MouseEnter(object sender, EventArgs e)
+        {
+            if (ButtonNo.Location.X == 10 && ButtonNo.Location.Y == 70)
+                ButtonNo.Location = new Point(10, 255);
+            else if (ButtonNo.Location.X == 10 && ButtonNo.Location.Y == 255)
+                ButtonNo.Location = new Point(455, 255);
+            else if (ButtonNo.Location.X == 455 && ButtonNo.Location.Y == 255)
+                ButtonNo.Location = new Point(455, 70);
+            else
+                ButtonNo.Location = new Point(10, 70);
+        }
+    }
+}
