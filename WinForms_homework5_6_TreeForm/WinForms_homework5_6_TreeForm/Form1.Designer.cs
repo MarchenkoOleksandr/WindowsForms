61af7cd5efa5a09eeaf2f36a7fe69f17079fa915
Aleksandr <38476175+MarchenkoOleksandr@users.noreply.github.com>
2018-06-21 20:49:30 +0300

    Add files via upload

---
+++ WinForms_homework5_6_TreeForm/WinForms_homework5_6_TreeForm/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace WinForms_homework5_6_TreeForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.labelClose = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // labelClose
+            // 
+            this.labelClose.AutoSize = true;
+            this.labelClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelClose.Location = new System.Drawing.Point(260, 10);
+            this.labelClose.Name = "labelClose";
+            this.labelClose.Size = new System.Drawing.Size(31, 29);
+            this.labelClose.TabIndex = 0;
+            this.labelClose.Text = "X";
+            this.labelClose.MouseClick += new System.Windows.Forms.MouseEventHandler(this.LabelClose_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LimeGreen;
+            this.ClientSize = new System.Drawing.Size(578, 544);
+            this.Controls.Add(this.labelClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelClose;
+    }
+}
+
---
+++ WinForms_homework5_6_TreeForm/WinForms_homework5_6_TreeForm/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Windows.Forms;
+
+//Особенности: помимо того, что крестиком можно закрыть форму, ёлку можно ещё и двигать мышью
+
+namespace WinForms_homework5_6_TreeForm
+{
+    public partial class Form1 : Form
+    {
+        Point mouseOffset;
+        bool IsMouseDown = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            GraphicsPath graphics = new GraphicsPath();
+            graphics.AddPolygon(
+                new[] {
+                    new Point(ClientSize.Width / 2, 30),
+                    new Point(ClientSize.Width / 2 + 130, 150),
+                    new Point(ClientSize.Width / 2 + 50, 150),
+                    new Point(ClientSize.Width / 2 + 180, 300),
+                    new Point(ClientSize.Width / 2 + 50, 300),
+                    new Point(ClientSize.Width / 2 + 50, 380),
+
+                    new Point(ClientSize.Width / 2 - 50, 380),
+                    new Point(ClientSize.Width / 2 - 50, 300),
+                    new Point(ClientSize.Width / 2 - 180, 300),
+                    new Point(ClientSize.Width / 2 - 50, 150),
+                    new Point(ClientSize.Width / 2 - 130, 150),
+                });
+            Region = new Region(graphics);
+            Brush bodyBrush = new SolidBrush(Color.Brown);
+            e.Graphics.FillRectangle(bodyBrush, new Rectangle(ClientSize.Width / 2 - 60, 270, 120, 120));
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (IsMouseDown)
+            {
+                Point point = MousePosition;
+                point.Offset(mouseOffset.X, mouseOffset.Y);
+                Location = point;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Point offset = new Point();
+            if (e.Button == MouseButtons.Left)
+            {
+                offset.X = -e.X - SystemInformation.FixedFrameBorderSize.Width;
+                offset.Y = -e.Y - SystemInformation.CaptionHeight - SystemInformation.FixedFrameBorderSize.Height;
+                mouseOffset = new Point(offset.X, offset.Y);
+                IsMouseDown = true;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+                IsMouseDown = false;
+        }
+
+        private void LabelClose_MouseClick(object sender, MouseEventArgs e)
+        {
+            Close();
+        }
+    }
+}
